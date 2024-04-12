175ccb3e45e9e8fa9615101b2bac858ec1802a0e
eason06021 <166780995+eason06021@users.noreply.github.com>
2024-04-12 11:39:34 +0800

    Create 9.reset_counter.php

---
+++ 9.reset_counter.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+    unset($_SESSION["counter"]);
+    echo "counter重置成功....";
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+
+?>
+
+可嘗試把session_start():註解，比較有何不同
